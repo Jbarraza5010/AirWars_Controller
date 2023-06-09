f3c38752edd69cf3b67c83823ed6ba7b2fa8412c
Javier Barraza <jbarraza5010@gmail.com>
2023-06-08 22:20:24 -0600

    Creacion de los aeropoertos y portaviones

--- src/main/java/com/example/airwars_controller/HelloController.java
+++ src/main/java/com/example/airwars_controller/HelloController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -13,11 +14,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import Classes.Server;
-import javafx.scene.text.Text;
+import Classes.Grafo;
+import Classes.LinkedList;
 
 public class HelloController implements Initializable {
 
     private Server server;
+
+    private String[] aeropuertos = {"norte america", "sur america", "europa", "asia", "oceania", "africa"};
+
+    private String[] portaviones = {"pacifico1", "pacifico2", "atlantico", "indico"};
 
     @FXML
     private Button eliminar;
@@ -46,6 +52,21 @@ public class HelloController implements Initializable {
             e.printStackTrace();
             System.out.println("Error creando server");
         }
+        LinkedList mapa = new LinkedList();
 
+        for(String i : portaviones){
+            int prob = ((int) (Math.random() * (3 - 1) + 1));
+            if (prob == 1){
+                mapa.insertFirst(i);
+            }
+        }
+
+        for(String i : aeropuertos){
+            int prob = ((int) (Math.random() * (3 - 1) + 1));
+            if (prob == 1){
+                mapa.insertFirst(i);
+            }
+        }
+        mapa.displayList();
     }
 }
