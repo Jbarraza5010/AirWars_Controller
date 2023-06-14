d75a9b481589f05c410d822487f434c96e13cd1c
Jbarraza5010 <102693990+Jbarraza5010@users.noreply.github.com>
2023-06-14 03:24:58 -0600

    Version final

--- src/main/java/Classes/Server.java
+++ src/main/java/Classes/Server.java
@@ -4,12 +4,19 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Clase del server
+ */
 public class Server {
     private ServerSocket serverSocket;
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
 
+    /**
+     * Clase constructora del server
+     * @param serverSocket
+     */
     public Server(ServerSocket serverSocket){
         try{
             this.serverSocket = serverSocket;
@@ -23,6 +30,10 @@ public class Server {
         }
     }
 
+    /**
+     * Metodo que envia un mensaje a la aplicacion del juego
+     * @param message
+     */
     public void sendMessageToController(String message){
         try{
             bufferedWriter.write(message);
@@ -35,6 +46,32 @@ public class Server {
         }
     }
 
+    /**
+     * Este metodo envia el tipo y la posicion del avion que se desea crear
+     * @param tipo
+     * @param pos
+     */
+    public void createPlane(String tipo, String pos){
+        try{
+            bufferedWriter.write(tipo);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            bufferedWriter.write(pos);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error enviando mensaje al controller");
+            closeAll(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    /**
+     * Mettodo que cierra el socket entre el server y el cliente
+     * @param socket
+     * @param bufferedReader
+     * @param bufferedWriter
+     */
     public void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
         try{
             if (bufferedReader != null){
--- src/main/java/com/example/airwars_controller/HelloController.java
+++ src/main/java/com/example/airwars_controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.airwars_controller;
 
+import Classes.Avion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -17,6 +20,9 @@ import Classes.Server;
 import Classes.Grafo;
 import Classes.LinkedList;
 
+/**
+ * Clase controller del controlador aereo
+ */
 public class HelloController implements Initializable {
 
     private Server server;
@@ -25,14 +31,45 @@ public class HelloController implements Initializable {
 
     private String[] portaviones = {"pacifico1", "pacifico2", "atlantico", "indico"};
 
+    private String[] todos = {"norte america", "sur america", "europa", "asia", "oceania", "africa", "pacifico1", "pacifico2", "atlantico", "indico"};
+
+    private Grafo grafo = new Grafo();
+
+    private int funcionando = 0;
+
+    private int derribados = 0;
+
+    private Avion SkyShredder = new Avion(3, 1, 1, 100);
+    private Avion GroundZero = new Avion(1, 3, 1, 100);
+    private Avion FlyingFortress = new Avion(1, 1, 3, 100);
+
+    private LinkedList aviones = new LinkedList();
+
+    private String[] tipos = {"SkyShredder", "GroundZero", "FlyingFortress"};
+
+    @FXML
+    private Text avionesD;
+
+    @FXML
+    private Text avionesF;
+
+    @FXML
+    private Button crear;
+
     @FXML
     private Button eliminar;
+
+    @FXML
+    private ChoiceBox<String> posicion;
 
     @FXML
     private ChoiceBox<?> rutas;
 
     @FXML
     private Text textAviones;
+
+    @FXML
+    private Text textBuscar;
 
     @FXML
     private Text textDestruidos;
@@ -43,7 +80,38 @@ public class HelloController implements Initializable {
     @FXML
     private Text textRuta;
 
+    @FXML
+    private ChoiceBox<String> tipo;
 
+    /**
+     * Este metodo crea un avion segun el tipo
+     * @param event
+     */
+    @FXML
+    void clickCrear(ActionEvent event) {
+        if (tipo.getValue() == "SkyShredder"){
+            server.createPlane("SkyShredder", posicion.getValue());
+            funcionando++;
+            avionesF.setText(String.valueOf(funcionando));
+            avionesD.setText(String.valueOf(derribados));
+        } else if (tipo.getValue() == "FlyingFortress") {
+            server.createPlane("FlyingFortress", posicion.getValue());
+            funcionando++;
+            avionesF.setText(String.valueOf(funcionando));
+            avionesD.setText(String.valueOf(derribados));
+        } else if (tipo.getValue() == "GroundZero") {
+            server.createPlane("GroundZero", posicion.getValue());
+            funcionando++;
+            avionesF.setText(String.valueOf(funcionando));
+            avionesD.setText(String.valueOf(derribados));
+        }
+    }
+
+    /**
+     * Metodo que incia el server junto con sus metodos
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try{
@@ -53,23 +121,34 @@ public class HelloController implements Initializable {
             System.out.println("Error creando server");
         }
 
-        LinkedList mapa = new LinkedList();
+        tipo.getItems().addAll(tipos);
 
         for(String i : portaviones){
             int prob = ((int) (Math.random() * (3 - 1) + 1));
+            int e = ((int) (Math.random() * (portaviones.length - 1) + 1));
+            String dest = portaviones[e];
+            int peso = ((int) (Math.random() * (5 - 1) + 1));
             if (prob == 1){
                 server.sendMessageToController(i);
-                mapa.insertFirst(i);
+                posicion.getItems().add(i);
+                //grafo.nuevoNodo(i);
+                //grafo.nuevoNodo(dest);
+                //grafo.NuevaArista(i, dest, peso);
             }
         }
 
         for(String i : aeropuertos){
             int prob = ((int) (Math.random() * (3 - 1) + 1));
+            int e = ((int) (Math.random() * (aeropuertos.length - 1) + 1));
+            String dest = aeropuertos[e];
+            int peso = ((int) (Math.random() * (5 - 1) + 1));
             if (prob == 1){
                 server.sendMessageToController(i);
-                mapa.insertFirst(i);
+                posicion.getItems().add(i);
+                //grafo.nuevoNodo(i);
+                //grafo.nuevoNodo(dest);
+                //grafo.NuevaArista(i, dest, peso);
             }
         }
-        mapa.displayList();
     }
 }
